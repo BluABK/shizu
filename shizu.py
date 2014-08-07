007416020229f5b17892b36a2ebb149cf9bbf6c2
BluABK <abk@blucoders.net>
2014-08-07 22:39:26 +0200

    samba.getlogins() - debug III

--- shizu.py
+++ shizu.py
@@ -190,7 +190,7 @@ def commands(usernick, msg, chan):
                 debug("Iteration: %s/%s" % (str(i), str(len(dbg))))
                 debug(dbg[i])
                 debug("Printing last item in list by force")
-                debug(dbg(len(dbg)))
+                debug(dbg[len(dbg)])
 
 
 def triggers(usernick, msg, chan, raw):
