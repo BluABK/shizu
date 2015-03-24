15451f6cac688b86217f989f5816f56156b882ff
BluABK <abk@blucoders.net>
2015-03-24 18:25:14 +0100

    String theory ~ (Regexp debug continues)

--- shizu.py
+++ shizu.py
@@ -297,10 +297,10 @@ def commands(usernick, msg, chan):
 def triggers(usernick, msg, chan):
     # TODO: Simplify pattern objects
     words_pat = re.compile(cfg.triggers_words(), flags=re.IGNORECASE)
-    msg_pat = re.compile(msg, flags=re.IGNORECASE)
-    nick_pat = re.compile(cfg.nick(), flags=re.IGNORECASE)
+    #msg_pat = re.compile(msg, flags=re.IGNORECASE)
+#    nick_pat = re.compile(cfg.nick(), flags=re.IGNORECASE)
 
-    matches = re.match(words_pat, nick_pat, msg_pat)
+    matches = re.match(words_pat, (cfg.nick() + msg))
     try:
         if matches.group(0) != "":  # If someone greets me, I will greet back.
             sendmsg((getgreeting(usernick)), chan)
