e622c549665ba5894dc5975aac537c1535c0ac68
BluABK <abk@blucoders.net>
2015-03-25 16:12:03 +0100

    changed traceback call III
    Surrounded by str()

--- shizu.py
+++ shizu.py
@@ -229,7 +229,7 @@ def commands(usernick, msg, chan):
                 except IndexError:
                     return
                 except:
-                    derp = traceback.print_exc()
+                    derp = str(traceback.print_exc())
                     sendmsg("A mysterious unexpected error occured" % chan)
                     sendmsg("%s" % derp, chan)
             else:
