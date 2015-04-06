2c02a67d689552964218353d2d09567deaf9d7d9
BluABK <abk@blucoders.net>
2015-04-06 19:36:03 +0200

    Created Last.FM module which currently relies heavily on the pylast Last.FM API handler

---
+++ modules/lastfm.py
@@ -0,0 +1,48 @@
+__author__ = 'BluABK <abk@blucoders.net'
+
+# This module requires pylast to be installed https://github.com/pylast/pylast
+import pylast
+import ConfigParser
+import os
+
+
+class Config:  # Shizu's config class
+    config = ConfigParser.RawConfigParser()
+
+    def __init__(self):
+        self.config.read(os.getcwd() + '/' + "config.ini")
+
+    def loadconfig(self):
+        configloc = os.getcwd() + '/' + "config.ini"
+        print(configloc)
+        self.config.read(configloc)
+        return True
+
+    def api_key(self):
+        return str(self.config.get('lastfm', 'api-key'))
+
+    def api_secret(self):
+        return str(self.config.get('lastfm', 'api-secret'))
+
+    def username(self):
+        return str(self.config.get('lastfm', 'username'))
+
+    def password(self):
+        return str(self.config.get('lastfm', 'password'))
+
+    def password_hash(self):
+        return str(self.config.get('lastfm', 'password_hash'))
+
+    def test_alias(self, user):
+        if str(self.config.get('lastfm', 'username')) == user:
+            return str(self.config.get('lastfm', 'username'))
+        else:
+            return str(self.config.get('lastfm-alias', user))
+
+cfg = Config()
+network = pylast.LastFMNetwork(api_key=cfg.api_key(), api_secret=cfg.api_secret(),
+                               username=cfg.username(), password_hash=cfg.password_hash())
+
+
+def now_playing(user):
+    return network.get_user(cfg.test_alias(user)).get_now_playing()
