77592cff134825f3aab35c9977caae242a20bc78
BluABK <abk@blucoders.net>
2015-04-10 01:42:00 +0200

    Replaced loop with a str()

--- shizu.py
+++ shizu.py
@@ -414,9 +414,7 @@ def commands(usernick, msg, raw_in, chan):
             if len(cmd) > 1:
                 if cmd[1] == "bio":
                     if len(cmd) > 2:
-                        test = lastfm.artist_bio(cmd[2])
-                        for item in xrange(len(test)):
-                            sendmsg(test[item], chan)
+                        sendmsg(str(lastfm.artist_bio(cmd[2])), chan)
                 elif cmd[1] == "recent":
                     default_num = 3
                     # !lastfm recent nick num
