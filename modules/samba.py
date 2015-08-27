b427726e730e5f069e1df4bbb84c9abaf4c92b66
BluABK <abk@blucoders.net>
2015-08-27 18:53:43 +0200

    Started work on FileLock class - update

--- modules/samba.py
+++ modules/samba.py
@@ -95,7 +95,7 @@ class Playback:
 
 class FileLock:
     def __init__(self):
-        self.dump = check_output("sudo smbstatus -L -d 11")
+        self.dump = check_output("sudo smbstatus -L -d 11", shell=True)
         self.locks = self.dump.split("parse_share_modes:")
 
     def get_amount(self):
