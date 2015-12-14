8708f8e1215e472c1cf59e674281a311b7b9e274
BluABK <abk@blucoders.net>
2015-12-15 00:59:20 +0100

    Removed misplaced try except

--- shizu.py
+++ shizu.py
@@ -63,10 +63,7 @@ if module_exists("modules.youtube") is True:
 #    stats.my_colour = clr
 #    clr_selection.append(clr)
 if module_exists("weather"):
-    try:
         import weather as yr
-    except Exception as shenanigans:
-        print shenanigans
 
 # Global variables
 my_name = os.path.basename(__file__).split('.', 1)[0]
