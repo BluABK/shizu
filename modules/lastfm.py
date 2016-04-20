e36f2479c606b89bc09a9dc27ae7d68008707496
BluABK <abk@blucoders.net>
2016-04-20 20:23:11 +0200

    lastfm.py: Added long-awaited documentation, refactored, reformatted and cleaned up functions etc..  #Springcleaning

--- modules/lastfm.py
+++ modules/lastfm.py
@@ -1,11 +1,11 @@
-__author__ = 'BluABK <abk@blucoders.net'
-
 import ConfigParser
+import cgi
 import os
 import re
-import cgi
 
 import colours as clr
+
+__author__ = 'BluABK <abk@blucoders.net'
 
 
 def module_exists(module_name):
@@ -34,31 +34,52 @@ class Config:  # Shizu's config class
     config = ConfigParser.RawConfigParser()
 
     def __init__(self):
+        """
+        Shizu module config class
+        :return:
+        """
         print "%s[%s]%s:\t Initiating config..." % (my_colour, my_name, clr.off)
         self.config.read(os.getcwd() + '/' + "config.ini")
 
-    def loadconfig(self):
+    def load(self):
+        """
+        Loads configuration file 'config.ini'
+        :return:
+        """
         configloc = os.getcwd() + '/' + "config.ini"
         print(configloc)
         self.config.read(configloc)
         return True
 
-    def api_key(self):
+    def get_api_key(self):
+        """
+        Last FM API http://www.last.fm/api
+        :return:
+        """
         return str(self.config.get('lastfm', 'api-key'))
 
-    def api_secret(self):
+    def get_api_secret(self):
+        """
+        Last FM API http://www.last.fm/api
+        :return:
+        """
         return str(self.config.get('lastfm', 'api-secret'))
 
-    def username(self):
+    def get_username(self):
         return str(self.config.get('lastfm', 'username'))
 
-    def password(self):
+    def get_password(self):
         return str(self.config.get('lastfm', 'password'))
 
-    def password_hash(self):
+    def get_password_hash(self):
         return str(self.config.get('lastfm', 'password_hash'))
 
     def test_alias(self, user):
+        """
+        Tests if the supplied user is a local alias
+        :param user:
+        :return:
+        """
         if str(self.config.get('lastfm', 'username')) == user:
             return str(self.config.get('lastfm', 'username'))
         else:
@@ -68,6 +89,12 @@ class Config:  # Shizu's config class
                 return None
 
     def add_alias(self, nick, user):
+        """
+        Adds a lastfm user alias for a nickname
+        :param nick: IRC nickname
+        :param user: LastFM Username
+        :return:
+        """
         if self.config.has_option('lastfm-alias', nick) is False:
             try:
                 self.config.read('config.ini')
@@ -82,6 +109,11 @@ class Config:  # Shizu's config class
             return "Alias already exists"
 
     def del_alias(self, nick):
+        """
+        Removes a set nickname alias
+        :param nick: IRC Nickname
+        :return:
+        """
         if self.config.has_option('lastfm-alias', nick) is False:
             try:
                 self.config.read('config.ini')
@@ -96,6 +128,10 @@ class Config:  # Shizu's config class
             return "Alias already exists"
 
     def list_alias(self):
+        """
+        Retrieves a list of all aliases and their associated nicknames
+        :return:
+        """
         if self.config.has_section('lastfm-alias') is True:
             return str(self.config.items('lastfm-alias'))
         else:
@@ -105,11 +141,15 @@ class Config:  # Shizu's config class
 cfg = Config()
 commandsavail_short = "np, npt, alias"
 commandsavail = "imaginary, recent*, bio, status, alias"
-network = pylast.LastFMNetwork(api_key=cfg.api_key(), api_secret=cfg.api_secret(),
-                               username=cfg.username(), password_hash=cfg.password_hash())
+network = pylast.LastFMNetwork(api_key=cfg.get_api_key(), api_secret=cfg.get_api_secret(),
+                               username=cfg.get_username(), password_hash=cfg.get_password_hash())
 
 
 def test_connection():
+    """
+    Self-test for LastFM connectivity
+    :return: Authenticated user or pylast Error object
+    """
     # debug = list()
     # debug.append(network.get_user())
     try:
@@ -119,6 +159,11 @@ def test_connection():
 
 
 def test_playing(user):
+    """
+    Test if user exists (Saves you from pylast issues)
+    :param user:
+    :return:
+    """
     try:
         return network.get_user(user).get_now_playing()
     except pylast.WSError:
@@ -128,6 +173,12 @@ def test_playing(user):
 
 
 def format_basic(li):
+    """
+    Formats a sequence of pylast PlayedTrack objects
+    Format: <date>, <time>      <artist> - <track>
+    :param li: PlayedTrack[]
+    :return: Formatted list (UTF-8)
+    """
     f_li = list()
     for track in li:
         unicode_track = unicode(str(track.track), 'utf8')
@@ -137,11 +188,17 @@ def format_basic(li):
     return f_li
 
 
-def strip_biojunk(string):
+def strip_biojunk(string, limit):
+    """
+    Strips irrelevant formatting from a bio and caps it at 512 bytes length
+    :param limit: Max length of string
+    :param string: Artist biography summary
+    :return: Formatted string
+    """
     newstring = ""
     for char in string:
         # 512 bytes is more than enough of a summary (amount takes sendmsg() into account
-        if newstring.__sizeof__() > 493:
+        if newstring.__sizeof__() > limit - 19:
             newstring += "..."
             break
         # "read more..." and other junk usually happens after a few newlines
@@ -154,6 +211,11 @@ def strip_biojunk(string):
 
 
 def strip_html(data):
+    """
+    Strips HTML tags from pylast bio summary data
+    :param data: pylast.get_bio_summary(self, language=None)
+    :return: Formatted string
+    """
     tag_re = re.compile(r'(<!--.*?-->|<[^>]*>)')
 
     # Remove well-formed tags, fixing mistakes by legitimate users
@@ -164,6 +226,11 @@ def strip_html(data):
 
 
 def now_playing(user):
+    """
+    Takes a LastFM Username and returns what they're currently scrobbling
+    :param user: LastFM Username
+    :return: str
+    """
     try:
         u = cfg.test_alias(user)
         if u is None:
@@ -172,22 +239,6 @@ def now_playing(user):
             u = user
 
         try:
-            # These are not the hacks you are looking for, move along
-            # base = str(network.get_user(u).get_now_playing())
-            # print "DEBUG: base = %s" % base
-            # artist = base.split(' - ', 1)[0]
-            # title = base.split(' - ', 1)[1]
-            # album = network.get_album(artist, title).get_the_sodding_name()
-            # print "DEBUG: album mayhaps: %s" % album
-            # album = str(album)
-            # print "DEBUG: album mayhaps: %s" % album
-            # album = str(network.get_album(artist, title))
-            # album = str(network.get_album(artist, title).get_url())
-            # album = str(network.search_for_track(artist, title))
-            # print "DEBUG album maybe: %s" % album
-            # album = "THIS IS TOTALLY NOT A FILLER FOR TESTING PURPOSES OR ANYTHING, B-BAKA!"
-            # np = artist + " - " + album + " - " + title
-            # return np
             return network.get_user(u).get_now_playing()
         except IndexError:
             print ('%s[%s\t now_playing()]%s: Index out of range (timeout) for %s' % (my_colour, my_name, clr.off, u))
@@ -198,6 +249,12 @@ def now_playing(user):
 
 
 def recently_played(user, num):
+    """
+    Returns a formatted list of scrobbled items for supplied user with supplied length (contents permitting)
+    :param user: LastFM Username
+    :param num: int
+    :return:
+    """
     try:
         u = cfg.test_alias(user)
         if u is None:
@@ -208,8 +265,8 @@ def recently_played(user, num):
             rplist = network.get_user(u).get_recent_tracks(limit=int(num) + 1)
         except (AttributeError, ValueError) as e:
             return e.message
-        #    except pylast.WSError.details == "Rate limit exceeded":
-        #        return "Rate limit exceeded o0"
+            #    except pylast.WSError.details == "Rate limit exceeded":
+            #        return "Rate limit exceeded o0"
     except pylast.WSError:
         # err = "No user with that name was found"
         return None
@@ -220,7 +277,13 @@ def recently_played(user, num):
     return format_basic(rplist)
 
 
-def artist_bio(name):
+def artist_bio(name, limit=512):
+    """
+    Returns a formatted artist biography summary string with limited length
+    :param limit: Default value: 512
+    :param name: Artist name
+    :return:
+    """
     try:
         data = network.get_artist(name).get_bio_summary()
     except AttributeError:
@@ -234,11 +297,10 @@ def artist_bio(name):
         derp.append(str(e.status))
         for a in e.args:
             derp.append(str(a) + ", ")
-        # data = "There was an error or some shit, happy now SpyTec? (Translation: General Error)"
         return derp
     data = strip_html(data).encode('utf-8')
     try:
-        data = strip_biojunk(data)
+        data = strip_biojunk(data, limit)
     except AttributeError:
         data = "Ouch, attribute error. Did you try something nasty?"
         return data
@@ -247,15 +309,30 @@ def artist_bio(name):
 
 
 def add_alias(nick, user):
+    """
+    Adds IRC nickname alias for LastFM Username
+    :param nick: IRC Nickname
+    :param user: LastFM Username
+    :return: boolean
+    """
     return cfg.add_alias(nick, user)
 
 
 def del_alias(nick):
+    """
+    Removes IRC nickname alias for LastFM Username
+    :param nick: IRC Nickname
+    :return: boolean
+    """
     return cfg.del_alias(nick)
 
 
-
 def helpcmd(cmdsym):
+    """
+    Shizu module helper
+    :param cmdsym: Command trigger symbol
+    :return: Filled in list of available commands
+    """
     cmdlist = list()
     cmdlist.append("Syntax: %scommand help arg1..argN" % cmdsym)
     cmdlist.append("Available commands: %s (* command contains sub-commands)" % commandsavail_short)
